0ed8e905a4894c50fd2bad936cd3798a6fcb7bfd
biganth <anthony@oop.io>
2015-10-07 14:40:39 -0400

    init

---
+++ src/Aurelia/wwwroot/app.js
@@ -0,0 +1,10 @@
+﻿export class App {  
+    configureRouter(config, router){
+        config.title = 'Aurelia-Setup';
+        config.map([
+          { route: ['','main'], name: 'main',      moduleId: './main',      nav: true, title:'Main Page' }
+        ]);
+
+        this.router = router;
+    }
+}
---
+++ src/Aurelia/wwwroot/main.js
@@ -0,0 +1,3 @@
+﻿export class Main{  
+    message = 'Hello from Aurelia';
+}
